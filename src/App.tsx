2c3af8b064b67c5ae1e06a46526096055114a70e
nbabcock <nathan.r.babcock@gmail.com>
2021-03-21 16:32:58 -0500

    Switch to official @react-three/cannon bindings

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import * as CANNON from 'cannon';
+import { Physics, useBox, usePlane } from '@react-three/cannon';
+import { PointerLockControls } from '@react-three/drei';
 import React, { useEffect, useState } from "react";
 import { Canvas } from 'react-three-fiber';
-import { Provider, useCannon } from "./useCannon";
 
-function Plane({ position }: any) {
+function Plane(props: any) {
   // Register plane as a physics body with zero mass
-  const ref = useCannon({ mass: 0 }, (body: any) => {
-    body.addShape(new CANNON.Plane())
-    body.position.set(...position)
-  })
+  const [ref] = usePlane(() => ({
+    rotation: [0, 0, 0],
+    type: 'Static',
+    ...props,
+  }))
   return (
     <mesh ref={ref} receiveShadow>
       <planeBufferGeometry attach="geometry" args={[1000, 1000]} />
@@ -17,12 +18,9 @@ function Plane({ position }: any) {
   )
 }
 
-function Box({ position }: any) {
+function Box(props: any) {
   // Register box as a physics body with mass
-  const ref = useCannon({ mass: 100000 }, (body: any) => {
-    body.addShape(new CANNON.Box(new CANNON.Vec3(1, 1, 1)))
-    body.position.set(...position)
-  })
+  const [ref] = useBox(() => ({ mass: 1, ...props }))
   return (
     <mesh ref={ref} castShadow receiveShadow>
       <boxGeometry attach="geometry" args={[2, 2, 2]} />
@@ -36,6 +34,14 @@ export default function App() {
   // When React removes (unmounts) the upper plane after 5 sec, objects should drop ...
   // This may seem like magic, but as the plane unmounts it removes itself from cannon and that's that
   useEffect(() => void setTimeout(() => set(false), 5000), [])
+  useEffect(() => {document.addEventListener('keydown', (event: KeyboardEvent) => {
+    switch ( event.code ) {
+      case 'ArrowUp':
+      case 'KeyW':
+        console.log('move forward')
+        break;
+    }
+  })})
 
   return <Canvas
     camera={{
@@ -44,9 +50,10 @@ export default function App() {
       far: 100000,
     }}
   >
+    <PointerLockControls />
     <ambientLight intensity={0.5} />
     <spotLight intensity={0.6} position={[30, 30, 50]} angle={0.2} penumbra={1} castShadow />
-    <Provider>
+    <Physics gravity={[0, 0, -25]}>
       <Plane position={[0, 0, -10]} />
       {showPlane && <Plane position={[0, 0, 0]} />}
       <Box position={[1, 0, 1]} />
@@ -56,6 +63,6 @@ export default function App() {
       <Box position={[-2, 2, 13]} />
       <Box position={[2, -1, 13]} />
       {!showPlane && <Box position={[0.5, 1.0, 20]} />}
-    </Provider>
+    </Physics>
   </Canvas>
 }
