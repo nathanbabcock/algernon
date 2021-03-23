774463454b317efff820a585b5e8a47f4452ddfc
nbabcock <nathan.r.babcock@gmail.com>
2021-03-23 12:54:01 -0500

    Minimal working collision with octrees

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
-import { Plane, PointerLockControls } from '@react-three/drei';
-import React from 'react';
-import { Canvas } from 'react-three-fiber';
+import { Box, Plane } from '@react-three/drei'
+import React, { useRef } from 'react'
+import { Canvas } from 'react-three-fiber'
+import { Group } from 'three'
+import FPSControls from './FPSControls'
 
 export default function App() {
-  return <Canvas
-    shadowMap
-    camera={{
-      position: [0, 0, 15],
-      near: 0.1,
-      far: 100000,
-    }}
-  >
-    
-    {/* <OrbitControls/> */}
-    <PointerLockControls/>
-    <ambientLight intensity={0.5} />
-    <spotLight intensity={0.6} position={[30, 30, 50]} angle={0.2} penumbra={1} castShadow />
-    
-    <Plane position={[0, 0, 0]} args={[100, 100]}/>
-  </Canvas>
+  const collisionObjects = useRef<Group>()
+
+  return (
+    <Canvas
+      shadowMap
+      camera={{
+        position: [0, 0, 15],
+        near: 0.1,
+        far: 100000,
+      }}
+    >
+      {/* <OrbitControls/> */}
+      <FPSControls collisionObjects={collisionObjects.current}/>
+      <ambientLight intensity={0.5} />
+      <spotLight intensity={0.6} position={[30, 30, 50]} angle={0.2} penumbra={1} castShadow />
+      
+      <group ref={collisionObjects}>
+        <Plane position={[0, 0, -100]} args={[100, 100]} receiveShadow/>
+
+        <Box position={[2, 2, -100]} args={[2, 2, 2]} castShadow receiveShadow>
+          <meshLambertMaterial color="red"/>
+        </Box>
+      </group>
+    </Canvas>
+  )
 }
---
+++ src/FPSControls.tsx
@@ -0,0 +1,96 @@
+import { useEffect } from 'react'
+import { useFrame, useThree } from 'react-three-fiber'
+import { Clock, Object3D, Vector3 } from 'three'
+import { Capsule } from 'three/examples/jsm/math/Capsule'
+import { Octree } from 'three/examples/jsm/math/Octree'
+
+export default function FPSControls(props: { collisionObjects?: Object3D }) {
+  const GRAVITY = 10
+  const MOUSE_SENSITIVITY = 500
+  const clock = new Clock()
+  const worldOctree = new Octree()
+  
+  const { camera, scene } = useThree()
+  setTimeout(() => worldOctree.fromGraphNode(scene), 100)
+
+  const playerCollider = new Capsule(new Vector3(0, 0.35, 0), new Vector3(0, 1, 0), 0.35)
+  const playerVelocity = new Vector3()
+  const playerDirection = new Vector3()
+
+  let playerOnFloor = false
+  const keyStates: any = {}
+
+  useEffect(() => {
+    document.addEventListener('keydown', (event: KeyboardEvent) => keyStates[ event.code ] = true)
+    document.addEventListener('keyup', (event: KeyboardEvent) => keyStates[ event.code ] = false)
+    document.addEventListener('mousedown', () => document.body.requestPointerLock())
+    document.body.addEventListener('mousemove', (event: MouseEvent) => {
+      if (document.pointerLockElement !== document.body) return
+      // camera.rotation.y -= event.movementX / MOUSE_SENSITIVITY
+      camera.rotation.x -= event.movementY / MOUSE_SENSITIVITY
+    })
+  })
+
+  const playerCollisions = () => {
+    const result = worldOctree.capsuleIntersect(playerCollider)
+    playerOnFloor = false
+    if (!result) return
+    playerOnFloor = result.normal.z > 0
+    if (!playerOnFloor)
+      playerVelocity.addScaledVector(result.normal, - result.normal.dot(playerVelocity))
+    playerCollider.translate(result.normal.multiplyScalar(result.depth))
+  }
+
+  const updatePlayer = (deltaTime: number) => {
+    if (playerOnFloor) {
+      const damping = Math.exp(- 3 * deltaTime) - 1
+      playerVelocity.addScaledVector(playerVelocity, damping)
+    } else {
+      playerVelocity.z -= GRAVITY * deltaTime
+    }
+
+    const deltaPosition = playerVelocity.clone().multiplyScalar( deltaTime )
+    playerCollider.translate(deltaPosition)
+    playerCollisions()
+    camera.position.copy(playerCollider.end)
+  }
+
+  const getForwardVector = () => {
+    camera.getWorldDirection(playerDirection)
+    playerDirection.z = 0
+    playerDirection.normalize()
+    return playerDirection
+  }
+
+  const getSideVector = () => {
+    camera.getWorldDirection( playerDirection )
+    playerDirection.z = 0
+    playerDirection.normalize()
+    playerDirection.cross( camera.up )
+    return playerDirection
+  }
+
+  const controls = (deltaTime: number) => {
+    const speed = 25
+    if (playerOnFloor) {
+      if (keyStates['KeyW'])
+        playerVelocity.add( getForwardVector().multiplyScalar(speed * deltaTime))
+      if (keyStates['KeyS'])
+        playerVelocity.add( getForwardVector().multiplyScalar(-speed * deltaTime))
+      if (keyStates['KeyA'])
+        playerVelocity.add( getSideVector().multiplyScalar(-speed * deltaTime))
+      if (keyStates['KeyD'])
+        playerVelocity.add(getSideVector().multiplyScalar(speed * deltaTime))
+      if (keyStates['Space']) 
+        playerVelocity.z = 15
+    }
+  }  
+
+  useFrame(() => {
+    const deltaTime = Math.min(0.1, clock.getDelta())
+    controls(deltaTime)
+    updatePlayer(deltaTime)
+  })
+
+  return null
+}
