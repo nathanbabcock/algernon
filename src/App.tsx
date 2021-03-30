13ba1fe3e1b7533bdf554cec42c4719bc615e10e
nbabcock <nathan.r.babcock@gmail.com>
2021-03-29 19:02:49 -0500

    Bring back cannonjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
-import { Plane } from '@react-three/drei'
-import React, { Suspense, useRef } from 'react'
+import { Physics, usePlane } from '@react-three/cannon'
+import React, { Suspense } from 'react'
 import { Canvas } from 'react-three-fiber'
-import { Euler, Group, Vector3 } from 'three'
-import { Octree } from 'three/examples/jsm/math/Octree'
-import Infinite1DMaze from './components/infinite-1d-maze/Infinite1DMaze'
-import NoFutureNoPast from './components/no-future-no-past/NoFutureNoPast'
+import { Euler, Vector3 } from 'three'
 import FPSControls from './FPSControls'
 
-export default function App() {
-  const collisionObjects = useRef<Group>()
-
-  const octree = useRef<Octree>(new Octree())
-  const requestCollisionUpdate = () => {
-    setTimeout(() => {
-      octree.current = new Octree();
-      octree.current.fromGraphNode(collisionObjects.current!)
-    }, 100);
-  }
-
-  const setPaused = (paused: boolean) => {
-    if (paused)
-      document.getElementById('pause')!.classList.add('visible')
-    else
-      document.getElementById('pause')!.classList.remove('visible')
-  }
-  setPaused(document.pointerLockElement !== document.body)
-
+export default function App () {
   return (
     <Canvas
       shadowMap
@@ -37,7 +16,6 @@ export default function App() {
         up: new Vector3(0, 0, 1),
       }}
     >
-      <FPSControls octree={octree} requestCollisionUpdate={requestCollisionUpdate} setPaused={setPaused}/>
       <ambientLight intensity={0.5} />
       <spotLight
         intensity={0.6}
@@ -48,24 +26,56 @@ export default function App() {
         shadow-mapSize-height={16384}
         shadow-mapSize-width={16384}
       />
-      
-      <group ref={collisionObjects}>
-        <Plane position={[0, 0, 0]} args={[1000, 1000]} receiveShadow>
-          <meshPhongMaterial attach="material" color="grey"/>
-        </Plane>
 
-        {/* <Box position={[0, 0, -0.9]} args={[2, 2, 2]} castShadow receiveShadow>
-          <meshPhongMaterial attach="material" color="red"/>
-        </Box> */}
+      <mesh castShadow receiveShadow position={[5, 5, 5]}>
+        <boxBufferGeometry args={[1, 1, 1]}/>
+        <meshBasicMaterial color="green" wireframe/>
+      </mesh>
 
-        <Suspense fallback={null}>
-          {/* <NoFutureNoPast position={[-10, -10, 0]} rotation={[0, 0, Math.PI/2]} requestCollisionUpdate={requestCollisionUpdate}/> */}
-          {/* <NoFutureNoPast requestCollisionUpdate={requestCollisionUpdate}/> */}
-
-          {/* <Infinite1DMaze position={[0, 4, 0]} rotation={[0, 0, -Math.PI/2]} requestCollisionUpdate={requestCollisionUpdate}/> */}
-          <Infinite1DMaze requestCollisionUpdate={requestCollisionUpdate}/>
-        </Suspense>
-      </group>
+      <Physics gravity={[0, 0, -25]}>
+        <PhysicsWorld/>
+      </Physics>
     </Canvas>
   )
+}
+
+function GroundPlane() {
+  const [plane] = usePlane(() => ({ type: 'Static' }))
+
+  return <mesh ref={plane} receiveShadow>
+    <planeBufferGeometry args={[1000, 1000]}>
+      <meshPhongMaterial attach="material" color="grey"/>
+    </planeBufferGeometry>
+  </mesh>
+}
+
+export function PhysicsWorld() {
+  const requestCollisionUpdate = () => {
+    console.warn('becoming deprecated')
+  }
+
+  const setPaused = (paused: boolean) => {
+    if (paused)
+      document.getElementById('pause')!.classList.add('visible')
+    else
+      document.getElementById('pause')!.classList.remove('visible')
+  }
+  setPaused(document.pointerLockElement !== document.body)
+
+  return (<Suspense fallback={null}>
+
+    <FPSControls setPaused={setPaused}/>
+
+    <GroundPlane/>
+
+    {/* <Box position={[0, 0, -0.9]} args={[2, 2, 2]} castShadow receiveShadow>
+      <meshPhongMaterial attach="material" color="red"/>
+    </Box> */}
+    
+    {/* <NoFutureNoPast position={[-10, -10, 0]} rotation={[0, 0, Math.PI/2]} requestCollisionUpdate={requestCollisionUpdate}/> */}
+    {/* <NoFutureNoPast requestCollisionUpdate={requestCollisionUpdate}/> */}
+
+    {/* <Infinite1DMaze position={[0, 4, 0]} rotation={[0, 0, -Math.PI/2]} requestCollisionUpdate={requestCollisionUpdate}/> */}
+    {/* <Infinite1DMaze requestCollisionUpdate={requestCollisionUpdate}/> */}
+  </Suspense>)
 }
--- src/FPSControls.tsx
+++ src/FPSControls.tsx
@@ -1,52 +1,44 @@
-import { useEffect } from 'react'
+import { useCylinder } from '@react-three/cannon'
+import { useEffect, useState } from 'react'
 import { useFrame, useThree } from 'react-three-fiber'
-import { Clock, Vector3 } from 'three'
-import { Capsule } from 'three/examples/jsm/math/Capsule'
-import { Octree } from 'three/examples/jsm/math/Octree'
+import { Object3D, Vector3 } from 'three'
 
 type FPSControlsProps = {
-  octree: React.MutableRefObject<Octree>,
-  requestCollisionUpdate: any,
   setPaused: any
 }
 
 const PLAYER_HEIGHT = 1
 const PLAYER_CROUCH_HEIGHT = 0.5
+const PLAYER_RADIUS = 0.35
+
+const MOUSE_SENSITIVITY = 1000
 
 export default function FPSControls(props: FPSControlsProps) {
-  const GRAVITY = 15
-  const MOUSE_SENSITIVITY = 1000
-  const clock = new Clock()
-  
+  const cylinderArgs: [number, number, number, number] = [PLAYER_RADIUS, PLAYER_RADIUS, PLAYER_HEIGHT, 32]
+  const [playerCylinder, cylinderBody] = useCylinder(() => ({ mass: 1, angularDamping: 1, args: cylinderArgs, rotation: [Math.PI/2, 0, 0] }))
+
   const { camera } = useThree()
-  useEffect(() => void(setTimeout(() => props.requestCollisionUpdate()), 100))
 
-  const playerCollider = new Capsule(new Vector3(0, 0, 0), new Vector3(0, 0, PLAYER_HEIGHT), 0.35)
-  const playerVelocity = new Vector3()
-  const playerDirection = new Vector3(0, 0, 0)
+  const [keyStates] = useState<any>({})
 
-  let playerOnFloor = false
-  const keyStates: any = {}
+  const [pitchObject] = useState(new Object3D())
+
+  const [yawObject] = useState(new Object3D())
+  yawObject.position.z = PLAYER_HEIGHT
+  yawObject.add(pitchObject)
 
   useEffect(() => {
-    camera.rotation.x = Math.PI/2
-    camera.rotation.z = 0
-
     const onKeydown = (event: KeyboardEvent) => keyStates[ event.code ] = true
     const onKeyup = (event: KeyboardEvent) => keyStates[ event.code ] = false
     const onMouseup = () => document.body.requestPointerLock()
     const onMousemove = (event: MouseEvent) => {
       if (document.pointerLockElement !== document.body) return
-      camera.rotation.z -= event.movementX / MOUSE_SENSITIVITY
+      camera.rotation.z += event.movementX / MOUSE_SENSITIVITY
       camera.rotation.x -= event.movementY / MOUSE_SENSITIVITY
     }
     const onPointerLockChange = () => {
       const paused = document.pointerLockElement !== document.body
       props.setPaused(paused)
-      // if (!paused)
-      //   document.body.requestFullscreen()
-      // else
-      //   document.exitFullscreen()
     }
 
     document.addEventListener('mousemove', onMousemove)
@@ -64,31 +56,8 @@ export default function FPSControls(props: FPSControlsProps) {
     }
   })
 
-  const playerCollisions = () => {
-    const result = props.octree.current.capsuleIntersect(playerCollider)
-    playerOnFloor = false
-    if (!result) return
-    playerOnFloor = result.normal.z > 0
-    if (!playerOnFloor)
-      playerVelocity.addScaledVector(result.normal, - result.normal.dot(playerVelocity))
-    playerCollider.translate(result.normal.multiplyScalar(result.depth))
-  }
-
-  const updatePlayer = (deltaTime: number) => {
-    if (playerOnFloor) {
-      const damping = Math.exp(- 3 * deltaTime) - 1
-      playerVelocity.addScaledVector(playerVelocity, damping)
-    } else {
-      playerVelocity.z -= GRAVITY * deltaTime
-    }
-
-    const deltaPosition = playerVelocity.clone().multiplyScalar( deltaTime )
-    playerCollider.translate(deltaPosition)
-    playerCollisions()
-    camera.position.copy(playerCollider.end)
-  }
-
   const getForwardVector = () => {
+    const playerDirection = new Vector3()
     camera.getWorldDirection(playerDirection)
     playerDirection.z = 0
     playerDirection.normalize()
@@ -96,6 +65,7 @@ export default function FPSControls(props: FPSControlsProps) {
   }
 
   const getSideVector = () => {
+    const playerDirection = new Vector3()
     camera.getWorldDirection(playerDirection)
     playerDirection.z = 0
     playerDirection.normalize()
@@ -103,36 +73,34 @@ export default function FPSControls(props: FPSControlsProps) {
     return playerDirection
   }
 
-  const controls = (deltaTime: number) => {
-    const crouch = keyStates['ControlLeft'] || keyStates['KeyC']
-    if (crouch) {
-      playerCollider.end.setZ(playerCollider.start.z + PLAYER_CROUCH_HEIGHT)
-    } else {
-      playerCollider.end.setZ(playerCollider.start.z + PLAYER_HEIGHT)
-    }
-    const walk = keyStates['ShiftLeft']
-    const speedMultiplier = crouch || walk ? 0.25 : 1
-
-    const speed = 15
-    if (playerOnFloor) {
-      if (keyStates['KeyW'])
-        playerVelocity.add( getForwardVector().multiplyScalar(speed * deltaTime * speedMultiplier))
-      if (keyStates['KeyS'])
-        playerVelocity.add( getForwardVector().multiplyScalar(-speed * deltaTime * speedMultiplier))
-      if (keyStates['KeyA'])
-        playerVelocity.add( getSideVector().multiplyScalar(-speed * deltaTime * speedMultiplier))
-      if (keyStates['KeyD'])
-        playerVelocity.add(getSideVector().multiplyScalar(speed * deltaTime * speedMultiplier))
-      if (keyStates['Space']) 
-        playerVelocity.z = 7.5
-    }
-  }  
-
   useFrame(() => {
-    const deltaTime = Math.min(0.1, clock.getDelta())
-    controls(deltaTime)
-    updatePlayer(deltaTime)
+    const inputVelocity = new Vector3()
+    const MOVESPEED = 10
+
+    if (keyStates['KeyW'])
+      inputVelocity.add(getForwardVector().multiplyScalar(MOVESPEED))
+    if (keyStates['KeyS'])
+      inputVelocity.sub(getForwardVector().multiplyScalar(MOVESPEED))
+    if (keyStates['KeyA'])
+      inputVelocity.add(getSideVector().multiplyScalar(MOVESPEED))
+    if (keyStates['KeyD'])
+      inputVelocity.sub(getSideVector().multiplyScalar(MOVESPEED))
+
+    const cylinderPos = playerCylinder!.current!.position
+    camera.position.set(cylinderPos.x, cylinderPos.y, cylinderPos.z)
+
+    if (inputVelocity.x === 0 && inputVelocity.y === 0 && inputVelocity.z === 0)
+      return
+    
+    // inputVelocity.applyEuler(camera.rotation)
+    cylinderBody.velocity.set(inputVelocity.x, inputVelocity.y, inputVelocity.z)
+    // console.log(inputVelocity.x, inputVelocity.y, inputVelocity.z)
   })
 
-  return null
+  return (
+    <mesh ref={playerCylinder} receiveShadow>
+      <cylinderBufferGeometry args={cylinderArgs}/>
+      <meshBasicMaterial color="green" wireframe/>
+    </mesh>
+  )
 }
