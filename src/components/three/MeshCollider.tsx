8dfc850556963397179a9f6f90304215790501c5
nbabcock <nathan.r.babcock@gmail.com>
2021-04-02 14:41:39 -0500

    Complete early game map

--- src/components/three/MeshCollider.tsx
+++ src/components/three/MeshCollider.tsx
@@ -4,8 +4,8 @@ import { BufferGeometry, Euler, Material, Vector3 } from 'three'
 export type MeshColliderProps = {
   material: Material,
   geometry: BufferGeometry,
-  parentPosition: Vector3,
-  parentRotation: Euler,
+  position?: Vector3,
+  rotation?: Euler,
 }
 
 export default function MeshCollider(props: any) {
@@ -13,19 +13,21 @@ export default function MeshCollider(props: any) {
   const box = geometry.boundingBox!
   const dims = box.max.clone().sub(box.min)
   const pos = box.min.clone().add(dims.clone().multiplyScalar(0.5))
-  pos.add(props.parentPosition.clone()).applyEuler(props.parentRotation)
+  
+  let position = new Vector3()
+  let rotation = new Euler()
+  if (props.position instanceof Array) position.set(...(props.position as [number, number, number]))
+  if (props.rotation instanceof Array) rotation.set(...(props.rotation as [number, number, number]))
+  if (props.position instanceof Vector3) position.copy(props.position)
+  if (props.rotation instanceof Euler) rotation.copy(props.rotation)
+
+  pos.add(position)
 
   const [ref] = useBox(() => ({
     type: 'Static',
     args: [dims.x, dims.y, dims.z],
     position: [pos.x, pos.y, pos.z],
-    rotation: [props.parentRotation.x, props.parentRotation.y, props.parentRotation.z],
   }))
 
-  // The position is "baked into" the mesh (applied local transforms in blender)
-  // But it is applied AGAIN a second time by Cannonjs
-  // We'll reverse one of these
-  return <group position={pos.clone().negate()}>
-    <mesh ref={ref} material={props.material} geometry={props.geometry} castShadow receiveShadow/>
-  </group>
+  return <mesh ref={ref} material={props.material} geometry={props.geometry} castShadow receiveShadow/>
 };
