d0e87f3f3ac93bb80a7f0ba297e156d9eabb6589
nbabcock <nathan.r.babcock@gmail.com>
2021-04-03 20:28:44 -0500

    EarlyGame transition: Petal 8

--- src/components/early-game/EarlyGameMeta.tsx
+++ src/components/early-game/EarlyGameMeta.tsx
@@ -211,6 +211,28 @@ function createPetal7(id: number): Infinite1DMazeSegment {
   return petal
 }
 
+function createPetal8(id: number): Infinite1DMazeSegment {
+  const petal = new Infinite1DMazeSegment(new Vector3(), new Euler(), id)
+  petal.paused = true
+  petal.curIndex = 0
+  const straight = new MazeStraightSegment(new Vector3(96, 66, 0), new Euler(0, 0, 0), petal.curIndex++)
+  const corner = new MazeCornerSegment(new Vector3(96, 70, 0), new Euler(0, 0, -Math.PI/2), petal.curIndex++)
+  const deadend = new MazeDeadEndSegment(new Vector3(100, 70, 0), new Euler(0, 0, -Math.PI/2), petal.curIndex++)
+
+  straight.connections[0].connectedTo = corner
+  corner.connections[1].connectedTo = straight
+
+  corner.connections[0].connectedTo = deadend
+  deadend.connections[0].connectedTo = corner
+
+  petal.maze = [
+    straight,
+    corner,
+    deadend,
+  ]
+  return petal
+}
+
 /**
  * Holds the entire scripted early game sequence,
  * plus the 11 possible transition paths towards the Flower Room
@@ -226,6 +248,7 @@ export default function EarlyGameMeta(props: any) {
     createPetal5(curIndex++),
     createPetal6(curIndex++),
     createPetal7(curIndex++),
+    createPetal8(curIndex++),
   ] as Infinite1DMazeSegment[])
   const [stage3Complete, setStage3Complete] = useState(false)
   const { camera } = useThree()
